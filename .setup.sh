c1d272797ba0d7951b311c0eea60c98e28a1f66b
Benjamin Tovar <benjamin.tovarcis@gmail.com>
2015-11-17 16:33:20 -0600

    Gitignore added

--- .setup.sh
+++
@@ -1,5 +0,0 @@
-# download tutorials
-git clone https://github.com/TATABOX42/ipython-titanic-notebook.git
-git clone https://github.com/TATABOX42/recsys-tutorial.git
-# start notebook
-jupyter notebook --no-browser --ip=0.0.0.0
